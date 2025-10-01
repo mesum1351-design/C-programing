07b9aa50a46cf995e350158b54f8f6a066f5ca3c
Mesum Abbas <mesum1351@gmail.com>
2025-10-01 20:21:48 +0500

    Add files via upload

---
+++ C++ Programming/My first program.cpp
@@ -0,0 +1,10 @@
+//My first program in C++
+// Write a program to display hello word
+
+#include<iostream>// preprocesor dirctive add the headerfile of input/output
+using namespace std;// namespace is used to  hold a logical grouping 
+
+int main()// main fuction
+{  
+  cout<<"Hello word";
+}
