9a43d38bb0f3424cbd54138861a00255d633fe88
Mesum Abbas <mesum1351@gmail.com>
2025-10-01 20:44:40 +0500

    Add files via upload

---
+++ C++ Programming/peactice1.cpp
@@ -0,0 +1,19 @@
+
+/* Wrte a program in which ask user to 
+   his name andf age */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	string name;
+	
+	cout<<"Enter Your name:  ";
+	cin>>  name;
+	cout<<"Enter Your Age:  ";
+	cin>>age ;
+	cout<<"\n Hi"<<name<<" You are "<<age<<" Year old";
+	return 0;
+}
